07d9c9d29110c31a2a3907c8adb4b15a3ecef423
Kirill Kotliar <kkotliar@web3tech.ru>
2023-01-29 20:28:50 +0300

    fix errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common'
 import { ScheduleModule } from '@nestjs/schedule'
 import { AppController } from './app.controller'
-import { RewardsModule } from '../modules/services/rewards-service/rewards.module'
 
 @Module({
   imports: [
-    RewardsModule,
     ScheduleModule.forRoot(),
   ],
   controllers: [AppController],
